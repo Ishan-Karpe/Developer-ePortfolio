ef738d0fbf5f208f181fca6a51920b8e132198ea
Ishan-Karpe <ishan.k.abhijeet@gmail.com>
2025-07-13 22:34:41 +0530

    added skills schema

---
+++ schemaTypes/skill.ts
@@ -0,0 +1,31 @@
+import {defineField, defineType} from 'sanity';
+
+export const skills = defineType({
+    name: 'skills',
+    title: 'Skills',
+    type: 'document',
+    fields: [
+        defineField({
+            name: 'skillsList',
+            title: 'Skills List',
+            type: 'array',
+            of: [
+            {
+                type: 'object',
+                fields: [
+                    defineField({
+                        name: 'skillName',
+                        title: 'Skill Name',
+                        type: 'string',
+                        validation: Rule => Rule.required().min(1).max(50)
+                    }),
+                    defineField({
+                        name: 'iconClass',
+                        title: 'Icon Class',
+                        type: 'string',
+                        validation: Rule => Rule.required().min(1).max(50),
+                    }),
+                ],
+            },
+        ],
+})]})
