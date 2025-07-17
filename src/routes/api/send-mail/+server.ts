99b6b07b478352007099c8074c8b49a99cc61bd4
Ishan Karpe <ishan.k.abhijeet@gmail.com>
2025-07-17 19:51:59 +0530

    Update +server.ts

--- src/routes/api/send-mail/+server.ts
+++ src/routes/api/send-mail/+server.ts
@@ -2,10 +2,32 @@ import { SENDGRID_API_KEY } from "$env/static/private";
 import { json } from "@sveltejs/kit";
 import sgMail from "@sendgrid/mail";
 
-export async function POST({ request }) {
-    const data = await request.json();
-    console.log(data)
+sgMail.setApiKey(SENDGRID_API_KEY);
 
-    return json({
-        message: "Email sent successfully", emailSentSuccessfully: true})
+export async function POST({ request }) {
+    const {contactMail, contactName, informationAboutProject} = await request.json();
+
+    if (!contactMail || !contactName || !informationAboutProject) {
+        return json({message: "Invalid input", emailSentSuccessfully: false}, {status: 400});
+
+    }
+
+    const message = {
+        to: 'ishan.k.abhijeet@gmail.com',
+        from: 'ishankarpe10@icloud.com', //requires sendgrid verified sender
+        subject: 'Contact Form on my ePortfolio',
+        html: `Somebody used the contact form on your site. <br/>
+        Name: ${contactName},
+        Email: ${contactMail},
+        Information about the project: ${informationAboutProject}`,
+    };
+
+    try {
+        await sgMail.send(message);
+        return json({message: "Email sent successfully", emailSentSuccessfully: true});
+    } catch (error) {
+        console.error("Error sending email:", error);
+        
+        return json({message: "Error sending email", emailSentSuccessfully: false}, {status: 500});
+    }
 }
